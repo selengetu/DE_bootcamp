54a6573d19eb3c28003e45a3b063f125d9c31c17
selengetu <Selengetu@gmail.com>
2024-11-24 14:18:33 -0500

    changes

--- Dimensional Data Modeling/HW1/actors_ddl.sql
+++ Dimensional Data Modeling/HW1/actors_ddl.sql
@@ -8,12 +8,12 @@ CREATE TYPE films AS (
 CREATE TYPE quality_classes AS
 ENUM ('bad', 'average', 'good', 'star');
 
-create table actors (
+CREATE TABLE actors (
     actorid text,
     filmid text,
     current_year integer,
     quality_class quality_classes,
     films films [],
     is_active bool,
-    primary key (actorid, filmid, current_year)
+    PRIMARY KEY (actorid, filmid, current_year)
 );
