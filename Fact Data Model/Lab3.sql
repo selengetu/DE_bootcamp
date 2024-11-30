209bc4363c667b7ade2c193d2c0f293043926c3b
selengetu <Selengetu@gmail.com>
2024-11-30 17:57:12 -0500

    Lab 3 part 2

--- Fact Data Model/Lab3.sql
+++ Fact Data Model/Lab3.sql
@@ -10,43 +10,42 @@
 
 --DELETE FROM array_metrics
 
--- INSERT INTO array_metrics
--- WITH daily_aggregate AS (
--- 	SELECT
--- 		user_id,
--- 		DATE(event_time) AS date,
--- 		COUNT(1) AS num_site_hits
--- 	FROM events
--- 	WHERE DATE(event_time) = DATE('2023-01-01')
--- 	AND user_id IS NOT NULL
--- 	GROUP BY user_id, DATE(event_time)
--- ),
--- yesterday_array AS (
--- 	SELECT *
--- 	FROM array_metrics
--- 	WHERE month_start = DATE('2023-01-01')
--- )
--- SELECT
--- 	COALESCE( da.user_id, ya.user_id) AS user_id,
--- 	COALESCE(ya.month_start, DATE_TRUNC('month', da.date)) AS month_start,
--- 	'site_hits' AS metric_name,
--- 	CASE WHEN ya.metric_array IS NOT NULL THEN
--- 		ya.metric_array || ARRAY[COALESCE(da.num_site_hits,0)]
--- 		WHEN ya.metric_array IS NULL THEN
--- 			ARRAY_FILL(0, ARRAY[COALESCE (date - DATE(DATE_TRUNC('month', date)), 0)])
--- 				|| ARRAY[COALESCE(da.num_site_hits,0)]
--- 		END AS metric_array
--- FROM daily_aggregate da
--- FULL OUTER JOIN yesterday_array ya
---     ON da.user_id = ya.user_id
--- ON CONFLICT  (user_id, month_start, metric_name)
--- DO
--- 	UPDATE SET metric_array = EXCLUDED.metric_array;
+INSERT INTO array_metrics
+WITH daily_aggregate AS (
+	SELECT
+		user_id,
+		DATE(event_time) AS date,
+		COUNT(1) AS num_site_hits
+	FROM events
+	WHERE DATE(event_time) = DATE('2023-01-02')
+	AND user_id IS NOT NULL
+	GROUP BY user_id, DATE(event_time)
+),
+yesterday_array AS (
+	SELECT *
+	FROM array_metrics
+	WHERE month_start = DATE('2023-01-01')
+)
+SELECT
+	COALESCE( da.user_id, ya.user_id) AS user_id,
+	COALESCE(ya.month_start, DATE_TRUNC('month', da.date)) AS month_start,
+	'site_hits' AS metric_name,
+	CASE WHEN ya.metric_array IS NOT NULL THEN
+		ya.metric_array || ARRAY[COALESCE(da.num_site_hits,0)]
+		WHEN ya.metric_array IS NULL THEN
+			ARRAY_FILL(0, ARRAY[COALESCE (date - DATE(DATE_TRUNC('month', date)), 0)])
+				|| ARRAY[COALESCE(da.num_site_hits,0)]
+		END AS metric_array
+FROM daily_aggregate da
+FULL OUTER JOIN yesterday_array ya
+    ON da.user_id = ya.user_id
+ON CONFLICT  (user_id, month_start, metric_name)
+DO
+	UPDATE SET metric_array = EXCLUDED.metric_array;
 
--- SELECT cardinality(metric_array) , COUNT(1)
--- FROM array_metrics
--- GROUP BY 1;
-
+SELECT cardinality(metric_array) , COUNT(1)
+FROM array_metrics
+GROUP BY 1;
 
 WITH agg AS(
 SELECT metric_name,month_start, ARRAY[SUM(metric_array[1]), SUM(metric_array[2]), SUM(metric_array[3])] as summed_array
