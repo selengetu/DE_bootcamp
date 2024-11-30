66494cf4fee04c80c8a6b27103f6affad10fff8c
selengetu <Selengetu@gmail.com>
2024-11-30 11:14:32 -0500

    end of lab2

--- Fact Data Model/Lab2.sql
+++ Fact Data Model/Lab2.sql
@@ -1,44 +1,46 @@
 -- Select max(event_time),
 --        min(event_time)
--- from events
+--  from events
+--
+-- DROP TABLE users_cumulated;
+-- CREATE TABLE users_cumulated(
+-- 	user_id TEXT,
+-- 	dates_active DATE[], --the list of dates in the past where the user was active
+-- 	date DATE, --the current date for the user
+-- 	PRIMARY KEY (user_id, date)
+-- );
 
--- CREATE TABLE users_cumulated (
---     user_id BIGINT,
---     --List of the dates in the past where user was active
---     dates_active DATE,
---     --Current date for the user
---     date Date,
---     PRIMARY KEY (user_id, date)
+-- INSERT INTO users_cumulated
+-- WITH yesterday AS (
+-- 	SELECT *
+-- 	FROM users_cumulated
+-- 	WHERE date = DATE('2023-01-30')
+-- ), today AS (
+-- 	SELECT
+-- 		CAST(user_id AS TEXT) AS user_id,
+-- 		DATE(CAST(event_time AS TIMESTAMP)) AS date_active
+-- 	FROM events
+-- 	WHERE DATE(CAST(event_time AS TIMESTAMP)) = DATE('2023-01-31')
+-- 	AND user_id IS NOT NULL
+-- 	GROUP BY user_id , DATE(CAST(event_time AS TIMESTAMP))
 -- )
--- CASE WHEN y.dates_active IS NULL THEN ARRAY[t.date_active]
+--
+--
+-- SELECT
+-- 	COALESCE(t.user_id, y.user_id) AS user_id,
+-- 	CASE
+-- 		WHEN y.dates_active IS NULL THEN ARRAY[t.date_active]
 -- 		WHEN t.date_active IS NULL THEN y.dates_active
 -- 		ELSE ARRAY[t.date_active] || y.dates_active
 -- 		END
-
-INSERT INTO users_cumulated
-WITH yesterday AS (
-	SELECT *
-	FROM users_cumulated
-	WHERE date = DATE('2023-01-30')
-), today AS (
-	SELECT
-		 user_id,
-		DATE(CAST(event_time AS TIMESTAMP)) AS date_active
-	FROM events
-	WHERE DATE(CAST(event_time AS TIMESTAMP)) = DATE('2023-01-31')
-	AND user_id IS NOT NULL
-	GROUP BY user_id , DATE(CAST(event_time AS TIMESTAMP))
-)
-
-
-SELECT
-	COALESCE(t.user_id, y.user_id) AS user_id,
-	Null AS dates_active,
-	COALESCE(t.date_active, y.date + INTERVAL '1 day') AS date
-FROM today t
-FULL OUTER JOIN yesterday y
-ON t.user_id = y.user_id;
-
+-- 		AS dates_active,
+-- 	COALESCE(t.date_active, y.date + INTERVAL '1 day') AS date
+-- FROM today t
+-- FULL OUTER JOIN yesterday y
+-- ON t.user_id = y.user_id;
+--
+-- select * from users_cumulated
+-- where date = '2023-01-31';
 
 WITH users AS (
 	SELECT * FROM users_cumulated
@@ -58,15 +60,13 @@ CASE WHEN dates_active @> ARRAY[DATE(series_date)]
 	END AS placeholder_int_value,
 	*
 FROM users CROSS JOIN series
---WHERE user_id = '439578290726747300'
-)
-
+WHERE user_id = '439578290726747300')
 
 -- SELECT user_id,
 -- 		CAST(CAST (SUM(placeholder_int_value) AS BIGINT) AS BIT(32))
 -- FROM place_holder_ints
 -- GROUP BY user_id;
-
+-- --
 SELECT 	user_id,
 		CAST(CAST (SUM(placeholder_int_value) AS BIGINT) AS BIT(32)),
 		BIT_COUNT(CAST(CAST (SUM(placeholder_int_value) AS BIGINT) AS BIT(32))) > 0
